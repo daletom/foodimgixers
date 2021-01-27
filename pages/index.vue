c047dac969fb4b9d1c5554bcb94306b26e40d68d
daletom <tom@imgix.com>
2021-01-27 12:46:33 -0800

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <header>
+  <picture>
+    <source
+      media="(min-width: 2100px)"
+      srcset="https://tom.imgix.net/kitchen.jpg?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=2400&fit=crop&ar=3%3A0.8&txt64=V2hhdCdzIENvb2tpbmcgQXQgaW1naXg_&txt-color=fff&txt-align=middle%2Ccenter&txt-fit=max&txt-size=110&txt-line=2&txt-line-color=f11111"
+            >
+    <source
+      media="(min-width: 1800px)"
+      srcset="https://tom.imgix.net/kitchen.jpg?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=2100&fit=crop&ar=3%3A0.8&txt64=V2hhdCdzIENvb2tpbmcgQXQgaW1naXg_&txt-color=fff&txt-align=middle%2Ccenter&txt-fit=max&txt-size=100&txt-line=2&txt-line-color=f11111"
+            >
+    <source
+      media="(min-width: 1600px)"
+      srcset="https://tom.imgix.net/kitchen.jpg?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=1800&fit=crop&ar=3%3A0.8&txt64=V2hhdCdzIENvb2tpbmcgQXQgaW1naXg_&txt-color=fff&txt-align=middle%2Ccenter&txt-fit=max&txt-size=95&txt-line=2&txt-line-color=f11111"
+            >
+    <source
+      media="(min-width: 1400px)"
+      srcset="https://tom.imgix.net/kitchen.jpg?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=1600&fit=crop&ar=3%3A0.8&txt64=V2hhdCdzIENvb2tpbmcgQXQgaW1naXg_&txt-color=fff&txt-align=middle%2Ccenter&txt-fit=max&txt-size=90&txt-line=2&txt-line-color=f11111"
+            >
+    <source
+      media="(min-width: 1200px)"
+      srcset="https://tom.imgix.net/kitchen.jpg?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=1400&fit=crop&ar=3%3A0.8&txt64=V2hhdCdzIENvb2tpbmcgQXQgaW1naXg_&txt-color=fff&txt-align=middle%2Ccenter&txt-fit=max&txt-size=85&txt-line=2&txt-line-color=f11111"
+            >
+    <source
+      media="(min-width: 900px)"
+      srcset="https://tom.imgix.net/kitchen.jpg?ar=2%3A0.7&fit=crop&w=1200&blend-size=inherit&blend-mode=multiply&blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&txt64=V2hhdCdzIENvb2tpbmcgQXQgaW1naXg_&txt-color=fff&txt-align=middle%2Ccenter&txt-fit=max&txt-size=80&txt-line=2&txt-line-color=f11111"
+            >
+    <source
+      media="(min-width: 600px)"
+      srcset="https://tom.imgix.net/kitchen.jpg?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=900&fit=crop&ar=2%3A0.7&&txt64=Q29va2luZyBBdCBpbWdpeA&txt-color=fff&txt-align=middle%2Ccenter&txt-fit=max&txt-size=80&txt-line=2&txt-line-color=f11111"
+            >
+    <source
+      media="(min-width: 450px)"
+      src="https://tom.imgix.net/kitchen.jpg?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=600&fit=crop&ar=1%3A0.8&txt64=Q29va2luZyBBdCBpbWdpeA&txt-color=fff&txt-align=middle%2Ccenter&txt-fit=max&txt-size=80&txt-line=2&txt-line-color=f11111"
+            >
+    <source
+      media="(min-width: 1px)"
+      srcset="https://tom.imgix.net/kitchen.jpg?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=550&fit=crop&ar=1%3A0.6&txt64=Q29va2luZyBBdCBpbWdpeA&txt-color=fff&txt-align=middle%2Ccenter&txt-fit=max&txt-size=80&txt-line=2&txt-line-color=f11111"
+            >
+    <img src="https://tom.imgix.net/kitchen.jpg?blend64=aHR0cHM6Ly9kZW1vcy5pbWdpeC5uZXQvZGFyay1lbGxpcHNlLWdyYWRpZW50LmFpP2ZtPXBuZyZhcj0yOjE&blend-mode=multiply&blend-size=inherit&w=1200&fit=crop&ar=2%3A0.7&txt64=Q29va2luZyBBdCBpbWdpeA&txt-color=fff&txt-align=middle%2Ccenter&txt-fit=max&txt-size=80&txt-line=2&txt-line-color=f11111">
+  </picture>
+  
+      </header>
+    <ix-img
+          v-for="item in food.data"
+          :key="item"
+          :src="item.attributes.origin_path"
+          :imgixParams="{txt:item.attributes.custom_fields.Name, 
+          'txt-size':'25', 'txt-color':'white', 'txt-line-color': 'blue', 'txt-fit':'max',
+           'txt-line':'0.8', mark:'https://tom.imgix.net/' + item.attributes.custom_fields.Chef 
+           + '.jpg?fm=png&auto=format&mask=ellipse&fit=facarea&facepad=2.1&border=3,ffffff',
+           'mark-align':'top-right', ar:'3:4', fit:'crop', markw:'0.3'}"
+          width="320"
+          loading="lazy"
+          :alt="item.attributes.custom_fields.Name"
+          fixed
+          class="float-left p-4"
+        />
+  </div>
+</template>
+
+<script>
+import food from '~/static/data.json'
+import VueImgix from '~/plugins/vue-imgix.js'
+
+export default {
+  asyncData ({ params }) {
+    return { food }
+  }
+}
+</script>
+
+<style>
+
+</style>
